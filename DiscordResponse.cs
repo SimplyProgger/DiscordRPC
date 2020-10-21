24c28353214cd92788c94170f20098415791644f
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-10-21 23:24:25 +0500

    initializate

---
+++ DiscordResponse.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discord.RPC
+{
+    public enum DiscordResponse
+    {
+        No = 0,
+        Yes = 1,
+        Ignore = 2
+    }
+}
